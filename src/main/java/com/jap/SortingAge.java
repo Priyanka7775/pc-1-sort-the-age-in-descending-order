48f94ae7793ddcc06e9eaee1b9994658512731c7
bhakti.rathod <bhakti.rathod@stackroute.in>
2022-05-31 13:35:49 +0530

    boilerplate added

---
+++ src/main/java/com/jap/SortingAge.java
@@ -0,0 +1,20 @@
+package com.jap;
+
+public class SortingAge {
+
+    //Write logic to sort the age using bubble sort
+    public int[] getSortedAge(int [] ageArray){
+
+        return new int[0];
+
+    }
+
+    public static void main(String[] args) {
+        SortingAge sortingAge = new SortingAge();
+        //Declare and initialize integer array in the variable ageArray
+        int ageArray[] = null;
+        int sortedArray[] = sortingAge.getSortedAge(ageArray);
+        //use for loop to iterate through the 'sortedArray' to print the output
+
+    }
+}
